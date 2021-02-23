785b10c6a3664eae29b7dd8384f73bb9c2a94f0e
Morpheus158 <Morpheus158@centrum.cz>
2021-02-23 19:39:08 +0100

    [ADD] health system

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,11 +6,27 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 30f;
 
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
     private void Update()
     {
         //ship movement according to mouse position
         transform.Translate(- Vector3.up * Input.GetAxis("Vertical") * Time.deltaTime * speed);
-        transform.Translate(- Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
     }
 
     private void FixedUpdate()
@@ -21,5 +37,12 @@ public class PlayerController : MonoBehaviour
         Quaternion rotation = Quaternion.LookRotation(mouseDirection, Vector3.forward);
         transform.rotation = rotation;
         transform.eulerAngles = new Vector3(-90, transform.eulerAngles.y, 0);
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
     }
 }
